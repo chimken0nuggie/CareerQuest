c290b3fe6e4e9fa88bc20bc8ada9a13aa325fc95
chimken0nuggie <hana.suzuki@sjsu.edu>
2024-10-05 01:27:32 -0700

    👽greeting in progress

--- game/src/components/Greeting.js
+++ game/src/components/Greeting.js
@@ -1,6 +1,20 @@
+import Heading from "../miniComponents/Heading";
+import ProjectName from "../miniComponents/ProjectName";
+
 function Greeting() {
     return (
-        <p>aosdfjoasidf</p>
+        <div className='PageContent' style={{width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <ProjectName/>
+            <Heading title="Introduction"/>
+            <div className="occupation-avatar" style={{width: '30%', height:'auto', backgroundColor: 'red'}} >
+                <div className="left-content" style={{}} >
+
+                </div>
+                <div className="right-content" style={{}}>
+
+                </div>
+            </div>
+        </div>
     )
 }
 export default Greeting;
